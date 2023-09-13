e7c4d1f055867947c7ff125e570ef2368aaed055
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-09-12 23:51:07 -0300

    feat(listener): add TcpListener and stream data read function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::{net::{TcpListener, TcpStream}, io::{Write, Read}};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = match TcpListener::bind("127.0.0.1:3000") {
+        Ok(tcp_listener) => {
+            println!("Listening on port 3000");
+
+            tcp_listener
+        },
+        Err(error) => panic!("{:?}", error) 
+    };
+
+    for tcp_stream_result in listener.incoming() {
+        let mut stream = match tcp_stream_result {
+            Ok(buff) => buff,
+            Err(error) => panic!("{:?}", error)
+        };
+
+        let body = handle_stream(&mut stream);
+
+        println!("{:?}", body);
+    }
+}
+
+fn handle_stream(stream: &mut TcpStream) -> String {
+    println!("{:?}", stream.peer_addr());
+    let mut buff = [0; 1000];
+
+    match stream.read(&mut buff) {
+        Ok(bytes) => bytes,
+        Err(error) => panic!("{:?}", error)
+    };
+
+    let data = match String::from_utf8(buff.to_vec()) {
+        Ok(body) => body,
+        Err(error) => panic!("{:?}", error)
+    };
+
+    match stream.shutdown(std::net::Shutdown::Both) {
+        Ok(_) => (),
+        Err(error) => panic!("{:?}", error),
+    };
+
+    return data;
 }
